04120062fc18deedbbaa9c5251670b01be6d09fc
Laura Nesvat <laura.nesvat@gmail.com>
2017-02-05 19:15:57 +0200

    initial commit

---
+++ app/src/main/java/lt/laurencija/mainecoonquiz/QuizActivity.java
@@ -0,0 +1,18 @@
+package lt.laurencija.mainecoonquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Namuciai on 2017.02.05.
+ */
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_screen);
+    }
+
+}
